813a5636d579d59680633f4c977205149c49f9cb
Raihan Alf <reyy.alf@gmail.com>
2021-10-15 09:40:23 +0800

    file database

--- projecttm.sql
+++ projecttm.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 09, 2021 at 02:42 AM
+-- Generation Time: Oct 15, 2021 at 03:39 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.4
 
@@ -67,10 +67,11 @@ CREATE TABLE `dokumen` (
 --
 
 INSERT INTO `dokumen` (`id`, `nomor_data`, `tanggal`, `waktu`, `pengirim`, `kota_pengirim`, `tujuan`, `jenis_barang`, `security`, `ga`, `ob`, `penerima`, `bukti_terima`) VALUES
-(18, 0, '2021-10-04', '09:42:15', 'Trio Banjarmasin', 'Banjarmasin', 'Rama', 'Dokumen', 'Angga Radit', 'Rara', 'Eko', 'Andi', 'f3b25a01df53dbb419e6b03a76a72d7a.jpeg'),
-(19, 0, '2021-10-07', '09:42:24', 'Trio Tanjung', 'Tanjung', 'Risa', 'Paket', 'Angga Radit', 'Rara', 'Eko', 'Asian', 'a906baf13c729640c74e185d19bd343d.jpg'),
-(20, 0, '2021-10-07', '09:42:36', 'Suzuki', 'Jakarta', 'Andi', 'Paket', 'Angga Radit', 'Rara', 'Eko', 'Yudi', '10e157780a42e8c50bea24de05ba2467.jpeg'),
-(21, 0, '2021-10-07', '09:59:29', 'Delima Motor', 'Banjarmasin', 'Ali', 'Dokumen', 'Rahman', 'Rara', 'Ahmad', 'Zara', 'b11b620b4ca4202bfe2a878fc7617bab.jpeg');
+(18, 0, '2021-10-04', '09:42:15', 'Trio Banjarmasin', 'Banjarmasin', 'Rama', 'Dokumen', 'Angga Radit', 'Rara', 'Kean', 'Andi', 'f3b25a01df53dbb419e6b03a76a72d7a.jpeg'),
+(19, 0, '2021-10-04', '09:42:24', 'Trio Tanjung', 'Tanjung', 'Risa', 'Dokumen', 'Angga Radit', 'Zara', 'Eko', 'Asian', 'a906baf13c729640c74e185d19bd343d.jpg'),
+(20, 0, '2021-10-06', '09:42:36', 'Suzuki', 'Jakarta', 'Andi', 'Paket', 'Angga Radit', 'Rara', 'Eko', 'Asian', '10e157780a42e8c50bea24de05ba2467.jpeg'),
+(21, 0, '2021-10-01', '09:59:29', 'Delima Motor', 'Banjarmasin', 'Ali', 'Dokumen', 'Rahman', 'Rara', 'Eko', 'Andi', 'b11b620b4ca4202bfe2a878fc7617bab.jpeg'),
+(22, 0, '2021-10-04', '16:03:33', 'kasdas', 'wqet', 'asdas', 'Paket', 'Rahman', '', '', 'Yudi', '5025a4bedcb77815f996e0faf33c0623.jpeg');
 
 -- --------------------------------------------------------
 
@@ -105,7 +106,7 @@ CREATE TABLE `user` (
   `user_name` varchar(50) NOT NULL,
   `user_email` varchar(50) NOT NULL,
   `user_password` varchar(50) NOT NULL,
-  `user_level` enum('admin','security','receipt','book') NOT NULL,
+  `user_level` enum('admin','security','receipt','book','master') NOT NULL,
   `user_status` enum('1','0') NOT NULL,
   `create_at` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -115,12 +116,11 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`user_id`, `user_name`, `user_email`, `user_password`, `user_level`, `user_status`, `create_at`) VALUES
-(1, 'Yudi', 'ahmadyudi@gmail.com', 'yudi123', 'admin', '1', '2021-09-14'),
-(2, 'Angga Radit', 'anggaradit@gmail.com', 'angga123', 'security', '1', '2021-09-14'),
-(3, 'Rara', 'rara@gmail.com', 'rara123', 'receipt', '1', '2021-09-14'),
-(4, 'Muhammad Fandi', 'Andi@gmail.com', 'andi123', 'book', '1', '2021-09-14'),
-(5, 'Rahman', 'rahman@gmail.com', 'rahman123', 'security', '1', '2021-09-24'),
-(6, 'Kayn', 'kay@gmail.com', 'kay123', 'receipt', '1', '0000-00-00');
+(1, 'Master', 'master@gmail.com', 'master123', 'master', '1', '2021-09-14'),
+(2, 'Security', 'security@gmail.com', 'security123', 'security', '1', '2021-09-14'),
+(6, 'Receipt', 'receipt@gmail.com', 'receipt123', 'receipt', '1', '2021-09-28'),
+(7, 'Admin', 'admin@gmail.com', 'admin123', 'admin', '1', '2021-10-11'),
+(13, 'Book', 'book@gmail.com', 'book123', 'book', '1', '2021-10-15');
 
 --
 -- Indexes for dumped tables
@@ -164,13 +164,13 @@ ALTER TABLE `contact`
 -- AUTO_INCREMENT for table `dokumen`
 --
 ALTER TABLE `dokumen`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
